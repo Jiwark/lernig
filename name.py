ebcf33a712bd584c0427487fc167b00206d879e2
Jiwark <javadiiiikarimi09594@gmail.com>
2022-04-30 22:30:35 +0430

    my new exercises.

---
+++ name.py
@@ -0,0 +1,8 @@
+name = input(" inter your name : ")
+if name == "javad":
+    print("congratulations, you are a javad!")
+elif name == "reza":
+    print("Em nara tonam kobi")
+else:
+    # Line below is a part of else block
+    print("You are not javad")
